be6a13f0ddae8ede5a0fcc27a23ac2f54fcfd0ab
kaizerrr-lab <rafaelluismedina@gmail.com>
2026-05-21 12:08:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
